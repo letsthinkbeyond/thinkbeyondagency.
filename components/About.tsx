25f366b85faa2db060b80fc324eb84287e08eaa9
letsthinkbeyond <letsthinkbeyond.co@gmail.com>
2026-07-24 18:25:58 +0530

    poster

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import SectionTitle from "./SectionTitle";
-import posterImage from "../lib/poster.png";
+import posterImage from "../lib/posters.png";
 
 export default function About() {
   return (
